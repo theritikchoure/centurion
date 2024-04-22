a82d4ab4a7bd2d422937055b753c296b0288c650
Ritik Chourasiya <ritvikchoure65+git@gmail.com>
2024-04-22 11:27:34 +0530

    fix: certificate added, mobile number

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,246 +1,242 @@
-(function($) {
-    'use strict';
+(function ($) {
+  "use strict";
 
-    // Mean Menu JS
-    jQuery('.mean-menu').meanmenu({
-        meanScreenWidth: "991"
-    });
+  // Mean Menu JS
+  jQuery(".mean-menu").meanmenu({
+    meanScreenWidth: "991",
+  });
 
-    // Nice Select JS
-    $('select').niceSelect();
+  // Nice Select JS
+  $("select").niceSelect();
 
-    // Header Sticky, Go To Top JS JS
-    $(window).on('scroll', function() {
+  // Header Sticky, Go To Top JS JS
+  $(window).on("scroll", function () {
+    // Header Sticky JS
+    if ($(this).scrollTop() > 150) {
+      $(".navbar-area").addClass("is-sticky");
+    } else {
+      $(".navbar-area").removeClass("is-sticky");
+    }
 
-        // Header Sticky JS
-        if ($(this).scrollTop() > 150) {
-            $('.navbar-area').addClass("is-sticky");
-        } else {
-            $('.navbar-area').removeClass("is-sticky");
-        };
+    // Go To Top JS
+    // var scrolled = $(window).scrollTop();
+    // if (scrolled > 300) $(".go-top").addClass("active");
+    // if (scrolled < 300) $(".go-top").removeClass("active");
+  });
 
-        // Go To Top JS
-        var scrolled = $(window).scrollTop();
-        if (scrolled > 300) $('.go-top').addClass('active');
-        if (scrolled < 300) $('.go-top').removeClass('active');
+  // Testimonials Slider JS
+  $(".testimonials-slider").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    autoplayHoverPause: true,
+    navText: [
+      "<i class='flaticon-left-arrow'></i>",
+      "<i class='flaticon-right-arrow'></i>",
+    ],
+  });
 
-    });
+  // Partner Slider JS
+  $(".partner-slider").owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    autoplayHoverPause: true,
+    navText: [
+      "<i class='bx bx-chevron-left'></i>",
+      "<i class='bx bx-chevron-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 2,
+      },
+      576: {
+        items: 3,
+      },
+      768: {
+        items: 4,
+      },
+      1200: {
+        items: 5,
+      },
+    },
+  });
 
-    // Testimonials Slider JS
-    $('.testimonials-slider').owlCarousel({
+  // Related Product JS
+  $(".related-product").owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
         items: 1,
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        autoplay: true,
-        smartSpeed: 1000,
-        autoplayHoverPause: true,
-        navText: [
-            "<i class='flaticon-left-arrow'></i>",
-            "<i class='flaticon-right-arrow'></i>",
-        ],
+      },
+      576: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+
+  // // Click Event JS
+  // $('.go-top').on('click', function() {
+  //     $("html, body").animate({
+  //         scrollTop: "0"
+  //     }, 0);
+  // });
+
+  // FAQ Accordion JS
+  $(".accordion")
+    .find(".accordion-title")
+    .on("click", function () {
+      // Adds Active Class
+      $(this).toggleClass("active");
+      // Expand or Collapse This Panel
+      $(this).next().slideToggle("fast");
+      // Hide The Other Panels
+      $(".accordion-content").not($(this).next()).slideUp("fast");
+      // Removes Active Class From Other Titles
+      $(".accordion-title").not($(this)).removeClass("active");
     });
 
-    // Partner Slider JS
-    $('.partner-slider').owlCarousel({
-        loop: true,
-        margin: 30,
-        nav: false,
-        dots: false,
-        autoplay: true,
-        smartSpeed: 1000,
-        autoplayHoverPause: true,
-        navText: [
-            "<i class='bx bx-chevron-left'></i>",
-            "<i class='bx bx-chevron-right'></i>",
-        ],
-        responsive: {
-            0: {
-                items: 2,
-            },
-            576: {
-                items: 3,
-            },
-            768: {
-                items: 4,
-            },
-            1200: {
-                items: 5,
-            }
-        }
+  // Tabs JS
+  $(".tab ul.tabs").addClass("active").find("> li:eq(0)").addClass("current");
+  $(".tab ul.tabs li").on("click", function (g) {
+    var tab = $(this).closest(".tab"),
+      index = $(this).closest("li").index();
+    tab.find("ul.tabs > li").removeClass("current");
+    $(this).closest("li").addClass("current");
+    tab
+      .find(".tab_content")
+      .find("div.tabs_item")
+      .not("div.tabs_item:eq(" + index + ")")
+      .slideUp();
+    tab
+      .find(".tab_content")
+      .find("div.tabs_item:eq(" + index + ")")
+      .slideDown();
+    g.preventDefault();
+  });
+
+  // Preloader
+  jQuery(window).on("load", function () {
+    $(".preloader").addClass("preloader-deactivate");
+  });
+
+  // Others Option For Responsive JS
+  $(".others-option-for-responsive .dot-menu").on("click", function () {
+    $(".others-option-for-responsive .container .container").toggleClass(
+      "active"
+    );
+  });
+
+  // WOW JS
+  if ($(".wow").length) {
+    var wow = new WOW({
+      mobile: false,
     });
+    wow.init();
+  }
 
-    // Related Product JS
-    $('.related-product').owlCarousel({
-        loop: true,
-        margin: 30,
-        nav: false,
-        dots: false,
-        autoplay: true,
-        smartSpeed: 1000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            576: {
-                items: 2,
-            },
-            768: {
-                items: 3,
-            },
-            1200: {
-                items: 3,
-            }
-        }
-    });
+  // Product View Slider JS
+  var bigimage = $("#big");
+  var thumbs = $("#thumbs");
+  // Var Totalslides = 10;
+  var syncedSecondary = true;
 
-    // Click Event JS
-    $('.go-top').on('click', function() {
-        $("html, body").animate({
-            scrollTop: "0"
-        }, 0);
-    });
-
-    // FAQ Accordion JS
-    $('.accordion').find('.accordion-title').on('click', function() {
-        // Adds Active Class
-        $(this).toggleClass('active');
-        // Expand or Collapse This Panel
-        $(this).next().slideToggle('fast');
-        // Hide The Other Panels
-        $('.accordion-content').not($(this).next()).slideUp('fast');
-        // Removes Active Class From Other Titles
-        $('.accordion-title').not($(this)).removeClass('active');
-    });
-
-    
-
-    // Tabs JS
-    $('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
-    $('.tab ul.tabs li').on('click', function(g) {
-        var tab = $(this).closest('.tab'),
-            index = $(this).closest('li').index();
-        tab.find('ul.tabs > li').removeClass('current');
-        $(this).closest('li').addClass('current');
-        tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
-        tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
-        g.preventDefault();
-    });
-
-    // Preloader
-    jQuery(window).on('load', function() {
-        $('.preloader').addClass('preloader-deactivate');
+  bigimage
+    .owlCarousel({
+      items: 1,
+      slideSpeed: 2000,
+      nav: true,
+      autoplay: true,
+      dots: false,
+      loop: true,
+      responsiveRefreshRate: 200,
+      navText: [
+        "<i class='bx bx-left-arrow-alt'></i>",
+        "<i class='bx bx-right-arrow-alt'></i>",
+      ],
     })
+    .on("changed.owl.carousel", syncPosition);
 
+  thumbs
+    .on("initialized.owl.carousel", function () {
+      thumbs.find(".owl-item").eq(0).addClass("current");
+    })
+    .owlCarousel({
+      items: 5,
+      dots: false,
+      nav: false,
+      navText: [
+        "<i class='bx bx-left-arrow-alt'></i>",
+        "<i class='bx bx-right-arrow-alt'></i>",
+      ],
+      smartSpeed: 200,
+      slideSpeed: 500,
+      slideBy: 4,
+      responsiveRefreshRate: 100,
+    })
+    .on("changed.owl.carousel", syncPosition2);
 
-    // Others Option For Responsive JS
-    $(".others-option-for-responsive .dot-menu").on("click", function() {
-        $(".others-option-for-responsive .container .container").toggleClass("active");
-    });
+  function syncPosition(el) {
+    //if loop is set to false, then you have to uncomment the next line
+    //var current = el.item.index;
 
-    // WOW JS
-    if ($('.wow').length) {
-        var wow = new WOW({
-            mobile: false
-        });
-        wow.init();
+    //to disable loop, comment this block
+    var count = el.item.count - 1;
+    var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+
+    if (current < 0) {
+      current = count;
+    }
+    if (current > count) {
+      current = 0;
     }
 
-    // Product View Slider JS
-    var bigimage = $("#big");
-    var thumbs = $("#thumbs");
-    // Var Totalslides = 10;
-    var syncedSecondary = true;
-
-    bigimage
-        .owlCarousel({
-            items: 1,
-            slideSpeed: 2000,
-            nav: true,
-            autoplay: true,
-            dots: false,
-            loop: true,
-            responsiveRefreshRate: 200,
-            navText: [
-                "<i class='bx bx-left-arrow-alt'></i>",
-                "<i class='bx bx-right-arrow-alt'></i>",
-            ]
-        })
-        .on("changed.owl.carousel", syncPosition);
-
+    //to this
     thumbs
-        .on("initialized.owl.carousel", function() {
-            thumbs
-                .find(".owl-item")
-                .eq(0)
-                .addClass("current");
-        })
-        .owlCarousel({
-            items: 5,
-            dots: false,
-            nav: false,
-            navText: [
-                "<i class='bx bx-left-arrow-alt'></i>",
-                "<i class='bx bx-right-arrow-alt'></i>",
-            ],
-            smartSpeed: 200,
-            slideSpeed: 500,
-            slideBy: 4,
-            responsiveRefreshRate: 100
-        })
-        .on("changed.owl.carousel", syncPosition2);
+      .find(".owl-item")
+      .removeClass("current")
+      .eq(current)
+      .addClass("current");
+    var onscreen = thumbs.find(".owl-item.active").length - 1;
+    var start = thumbs.find(".owl-item.active").first().index();
+    var end = thumbs.find(".owl-item.active").last().index();
 
-    function syncPosition(el) {
-        //if loop is set to false, then you have to uncomment the next line
-        //var current = el.item.index;
-
-        //to disable loop, comment this block
-        var count = el.item.count - 1;
-        var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
-
-        if (current < 0) {
-            current = count;
-        }
-        if (current > count) {
-            current = 0;
-        }
-
-        //to this
-        thumbs
-            .find(".owl-item")
-            .removeClass("current")
-            .eq(current)
-            .addClass("current");
-        var onscreen = thumbs.find(".owl-item.active").length - 1;
-        var start = thumbs
-            .find(".owl-item.active")
-            .first()
-            .index();
-        var end = thumbs
-            .find(".owl-item.active")
-            .last()
-            .index();
-
-        if (current > end) {
-            thumbs.data("owl.carousel").to(current, 100, true);
-        }
-        if (current < start) {
-            thumbs.data("owl.carousel").to(current - onscreen, 100, true);
-        }
+    if (current > end) {
+      thumbs.data("owl.carousel").to(current, 100, true);
     }
-
-    function syncPosition2(el) {
-        if (syncedSecondary) {
-            var number = el.item.index;
-            bigimage.data("owl.carousel").to(number, 100, true);
-        }
+    if (current < start) {
+      thumbs.data("owl.carousel").to(current - onscreen, 100, true);
     }
-    thumbs.on("click", ".owl-item", function(e) {
-        e.preventDefault();
-        var number = $(this).index();
-        bigimage.data("owl.carousel").to(number, 300, true);
-    });
+  }
 
+  function syncPosition2(el) {
+    if (syncedSecondary) {
+      var number = el.item.index;
+      bigimage.data("owl.carousel").to(number, 100, true);
+    }
+  }
+  thumbs.on("click", ".owl-item", function (e) {
+    e.preventDefault();
+    var number = $(this).index();
+    bigimage.data("owl.carousel").to(number, 300, true);
+  });
 })(jQuery);
